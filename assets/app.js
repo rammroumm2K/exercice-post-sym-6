f53a27daae16c4f47442cf008e11ea8ea95a376c
rammroumm2K <ramybrz00@gmail.com>
2024-09-26 16:22:29 +0200

    reglage et ordre fichier

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-import './bootstrap.js';
+//import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -6,5 +6,9 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import './styles/styles.css';
+import './popper.min.js';
+import './bootstrap.min.js';
+import './styles/bootstrap.min.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
