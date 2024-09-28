20fe8aef808998ddfa72ed17a9f833e46dd91fdd
rammroumm2K <ramybrz00@gmail.com>
2024-09-28 17:21:00 +0200

    fin instruction read me

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SectionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,12 +10,14 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends AbstractController
 {
     
+
+    
     #[Route('/', name: 'homepage')]
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
             'title' => 'Playstation Post Blogger',
-            'homepage_text'=> "Nous somme le ".date('d/m/Y \à H:i'),
+            'homepage_text'=> "Welcome",
             
         ]);
     }
@@ -27,4 +30,28 @@ class MainController extends AbstractController
             'homepage_text'=> "Et je parle encore de moi !",
         ]);
     }
+
+     // création de l'url pour le détail d'une section
+     #[Route(
+        # chemin vers la section avec son id
+        path: '/section/{id}',
+        # nom du chemin
+        name: 'section',
+        # accepte l'id au format int positif uniquement
+        requirements: ['id' => '\d+'],
+        # si absent, donne 1 comme valeur par défaut
+        defaults: ['id'=>1])]
+
+    public function section(SectionRepository $sections, int $id): Response
+    {
+        // récupération de la section
+        $section = $sections->find($id);
+        return $this->render('main/section.html.twig', [
+            'title' => 'Section '.$section->getSectionTitle(),
+            'homepage_text'=> $section->getSectionDescription(),
+            'section' => $section,
+            'sections' => $sections->findAll(),
+        ]);
+    }
 }
+
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SectionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
